788485c568aaafa09bb0352087827c1974eaa135
developercyrus <developer.cyrus@gmail.com>
2015-06-09 21:23:00 +0800

    @i3300

--- struts-snippets/src/test/java/example1/helloworld/controller/HelloWorldActionIT.java
+++ struts-snippets/src/test/java/example1/helloworld/controller/HelloWorldActionIT.java
@@ -11,13 +11,13 @@ import org.junit.Test;
 
 public class HelloWorldActionIT {
 	@Test
-    public void test1() throws Exception {
+	public void test1() throws Exception {
 		String url = "http://localhost:8080/hello.do";
-        Document doc = Jsoup.parse(new URL(url), 5000);
-        Elements elements = doc.select("body");        
-        String expected = elements.get(0).text();
-                
-        assertEquals("Hello World", expected);
+		Document doc = Jsoup.parse(new URL(url), 5000);
+		Elements elements = doc.select("body");
+		String expected = elements.get(0).text();
+
+		assertEquals("Hello World", expected);
 	}
-	
+
 }
